a432b352ec8ebd1d58ea04e987761541d75ada62
Zane Schaffer <hello@zanes.house>
2021-06-14 23:23:12 -0700

    new additions

--- sketch.js
+++ sketch.js
@@ -4,39 +4,35 @@ let s = function (p) {
     fira = p.loadFont('assets/FiraCode-Retina.ttf');
 
   }
+
   p.setup = function () {
-    let mobile = 400;
-    if (p.windowWidth < mobile) {
-      canvas = p.createCanvas(p.windowWidth, p.windowHeight / 3, p.WEBGL);
-    } else {
-      canvas = p.createCanvas(p.windowWidth, p.windowHeight, p.WEBGL);
-    }
-    canvas.position(0, 0,)
+    var canvasDiv = document.getElementById('canvasContainer');
+    var width = canvasDiv.offsetWidth;
+    var height = canvasDiv.offsetHeight;
+    canvas = p.createCanvas(width, height, p.WEBGL);
+    canvas.position(0, 0, "relative")
     p.textFont(fira);
     p.frameRate(60)
 
   }
   p.draw = function () {
-    p.background(22)
+    p.background('#2E3440')
     p.textSize(p.windowWidth / 17)
     gap = p.windowWidth / 22
     p.push()
     p.translate(-p.width / 10, 0)
     time = p.millis()
-    if (p.mouseIsPressed) {
-      let a = p.atan2(p.mouseY - p.height / 2, p.mouseX - p.width / 2)
-      p.rotateX(a);
-      p.rotateY(a);
-      p.stroke(0, 200, 200)
-    } else {
-      p.rotateX(time * 0.0005)
-      p.rotateY(time * 0.0005)
-      p.stroke(255)
-    }
+
+    p.rotateX(time * 0.0005)
+    p.rotateY(time * 0.0005)
+    p.stroke(255)
+
     p.fill(0, 160, 255, 0)
     p.sphere((p.windowWidth / 6), 3, 3)
 
     p.pop()
+
+    p.fill('#D8DEE9')
 
     p.text('Zane', 0, -gap)
     p.text('Schaffer', 0, gap)
@@ -48,6 +44,6 @@ let s = function (p) {
   }
 
 }
-let header = new p5(s, 'container');
+let header = new p5(s, 'canvasContainer');
 
 
--- sketch2.js
+++ sketch2.js
@@ -1,5 +1,26 @@
-body = document.body;
+const canvasContainer = document.getElementById("canvasContainer");
 
-body.addEventListener('click', () => {
-    console.log(body.style)
-})
+// Left Section
+const workLink = document.getElementById("workLink");
+const workText = document.getElementById("workText");
+
+// Right Section
+const musicLink = document.getElementById("musicLink");
+const musicText = document.getElementById("musicText");
+
+/**
+* Changes an HTMLElement's display style between 'flex' and 'none'
+*/
+function toggleVis(link, text) {
+    link.addEventListener('click', () => {
+        if (text.style.display == 'flex' || !text.style.display) {
+            text.style.display = 'none'
+        } else {
+            text.style.display = 'flex'
+        }
+    })
+}
+
+
+toggleVis(workLink, workText);
+toggleVis(musicLink, musicText);
